a958fb9f971581352bf86b85bfffcb4dab330de9
vignesh <vickie1045@gmail.com>
2023-04-18 16:05:58 +0530

    added commemt

--- collections/non-generic/Stack.cs
+++ collections/non-generic/Stack.cs
@@ -5,20 +5,27 @@ using System.Collections;
 
 class Browser
 {
+    //defining statck class
    private Stack history ;
 
+    //initializing the stack within the constructor
    public Browser()
    {
     history = new Stack();
    }
 
+   //method to navigate to other websites
+
    public void NavigateTo(string Url)
    {
-        Console.WriteLine($"Navigatin To {Url}");
+        Console.WriteLine($"Navigating To {Url}");
         history.Push(Url);
    }
+   //method to navigate back to the previos window
    public void NavigateBack()
    {
+        //checking if there is any previous page exist
+
         if(history.Count==0)
         {
             Console.WriteLine("No More Pages To Go Back");
@@ -28,6 +35,7 @@ class Browser
         string url = (string)history.Peek();
         Console.WriteLine($"Navigating Back to {url}");
    }
+   //method to display the current web page
    public void CurrentWindow()
    {
         string currentWindow = (string)history.Peek();
@@ -35,14 +43,17 @@ class Browser
    }
 
 
-
-
 }
 class stack
 {
     static void Main(string[] args)
     {
+
+        //creating object for the Browser class
+
         Browser browser = new Browser();
+
+        //navigating to the new window
 
         browser.NavigateTo("www.google.com");
         browser.NavigateTo("www.facebook.com");
@@ -50,8 +61,11 @@ class stack
         browser.NavigateTo("www.Instagram.com");
         browser.NavigateTo("www.twitter.com");
 
+        //navigating back to the previous window
+
         browser.NavigateBack();
         browser.NavigateBack();
+
         browser.CurrentWindow();
         
     }
